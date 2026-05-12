f149100842839b5a35cdf2ed847d5621eaec8a9e
Egor Gut <egor_gut@outlook.fr>
2026-05-12 11:44:48 +0200

    Premiere version du jeu : perso qui bouge et c'est tout :)

---
+++ Assets/Scripts/chest.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class chest : MonoBehaviour, IInteractable 
+{
+    public bool CanInteract()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Interactable()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/move.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class move : MonoBehaviour
+{
+    [SerializeField] public Rigidbody2D rb;
+
+    [SerializeField] private Sprite spriteHaut;
+    [SerializeField] private Sprite spriteBas;
+    [SerializeField] private Sprite spriteGauche;
+    [SerializeField] private Sprite spriteDroite;
+
+    private float horizontal;
+    private float vertical;
+    private float speed = 5f;
+    private bool facingLeft = true;
+
+
+    void Update()
+    {
+        horizontal = 0f;
+        vertical = 0f;
+
+        if (Keyboard.current.leftArrowKey.isPressed || Keyboard.current.aKey.isPressed)
+        {
+            horizontal = -1f;
+        }
+        else if (Keyboard.current.rightArrowKey.isPressed || Keyboard.current.dKey.isPressed)
+        {
+            horizontal = 1f;
+        }
+        else if (Keyboard.current.upArrowKey.isPressed || Keyboard.current.wKey.isPressed)
+        {
+            vertical = 1f;
+        }
+        else if (Keyboard.current.downArrowKey.isPressed || Keyboard.current.sKey.isPressed)
+        {
+            vertical = -1f;
+        }
+
+        Flip();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(horizontal * speed, vertical * speed);
+    }
+
+    private void Flip()
+    {
+        if ((facingLeft && horizontal < 0f) || (!facingLeft && horizontal > 0f))
+        {
+            facingLeft = !facingLeft;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
+    }
+}
