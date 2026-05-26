f836843890e61eb39bf147bb2a8d8ec128f0927b
Egor Gut <egor_gut@outlook.fr>
2026-05-26 11:45:33 +0200

    test bien bien testeux

---
+++ Assets/Scripts/Trigger.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class Trigger : MonoBehaviour
+{
+    [SerializeField] private bool triggerActive = false;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            triggerActive = true;
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            triggerActive = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (triggerActive && Input.GetKeyDown(KeyCode.Space))
+        {
+            SomeCoolAction();
+        }
+    }
+
+    public void SomeCoolAction()
+    {
+        Debug.Log("YO BONJOUR");
+    }
+}
